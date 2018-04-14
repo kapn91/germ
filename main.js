d09c6cada7885b8fb156e9c16f700db93841059f
Beau Baker <beaubaker1027@gmail.com>
2018-04-14 11:46:13 -0500

    updated main process

--- main.js
+++ main.js
@@ -2,7 +2,28 @@ const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
 const url = require('url');
 
-app.on('ready', () => {
-  let win = new BrowserWindow({width: 1000, height: 800, resizable: false, frame: false});
-  win.loadURL(`file://${__dirname}/index.html`);
+let win;
+const index = `file://${__dirname}/index.html`
+
+function createWindow() {
+  win = new BrowserWindow({width: 1000, height: 800, resizable: false, frame: false});
+  win.loadURL(index);
+
+  win.on('closed', () => {
+    win = null;
+  })
+}
+app.on('ready', createWindow)
+
+
+app.on('window-all-cloases', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if(win === null) {
+    createWindow();
+  }
 })
