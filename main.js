7f7ce94e9e60d38421717321133c6246b1a030be
Beau Baker <beaubaker1027@gmail.com>
2018-07-14 20:38:53 -0500

    fixed app.quit for windows

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
 app.on('ready', createWindow)
 
 
-app.on('window-all-cloases', () => {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
